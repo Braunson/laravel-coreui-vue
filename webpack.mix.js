1872c0cf1ec8216531d9a10bafe7275992ba2151
Braunson Yager <braunson@gmail.com>
2017-04-01 15:27:11 -0400

    Added template assets + updated webpack.mix file
    - Added CoreUI template assets
    - Updated webpack.mix.js file, commented out 'copy code', someone can
    use later to update their code from the master repo if they choose to.
    -

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,18 +12,18 @@ var coreui_vendor = 'vendor/mrholek/CoreUI-Free-Bootstrap-Admin-Template/Vue_Ful
  |
  */
 
-// Copy over the CoreUI Assets
-mix.copyDirectory(coreui_vendor + '/static/img', 'public/static/img')
-   .copy(coreui_vendor + '/static/css/font-awesome.css', 'resource/assets/sass/coreui/fonts/font-awesome.css')
-   .copy(coreui_vendor + '/static/css/simple-line-icons.css', 'resource/assets/sass/coreui/fonts/simple-line-icons.css')
-   .copyDirectory(coreui_vendor + '/static/fonts', 'public/static/fonts')
-   .copyDirectory(coreui_vendor + '/scss', 'resources/assets/sass/coreui')
-   .copyDirectory(coreui_vendor + '/src/assets', 'resources/assets/js/assets')
-   .copyDirectory(coreui_vendor + '/src/components', 'resources/assets/js/components')
-   .copyDirectory(coreui_vendor + '/src/containers', 'resources/assets/js/containers')
-   .copyDirectory(coreui_vendor + '/src/router', 'resources/assets/js/router')
-   .copyDirectory(coreui_vendor + '/src/views', 'resources/assets/js/views')
-   .copy(coreui_vendor + '/src/App.vue', 'resources/assets/js/App.vue', false);
+// Copy over the CoreUI Assets initially
+// mix.copyDirectory(coreui_vendor + '/static/img', 'public/static/img')
+//    .copy(coreui_vendor + '/static/css/font-awesome.css', 'resources/assets/sass/coreui/fonts/font-awesome.css', false)
+//    .copy(coreui_vendor + '/static/css/simple-line-icons.css', 'resources/assets/sass/coreui/fonts/simple-line-icons.css', false)
+//    .copyDirectory(coreui_vendor + '/static/fonts', 'public/static/fonts')
+//    .copyDirectory(coreui_vendor + '/scss', 'resources/assets/sass/coreui')
+//    .copyDirectory(coreui_vendor + '/src/assets', 'resources/assets/js/assets')
+//    .copyDirectory(coreui_vendor + '/src/components', 'resources/assets/js/components')
+//    .copyDirectory(coreui_vendor + '/src/containers', 'resources/assets/js/containers')
+//    .copyDirectory(coreui_vendor + '/src/router', 'resources/assets/js/router')
+//    .copyDirectory(coreui_vendor + '/src/views', 'resources/assets/js/views')
+//    .copy(coreui_vendor + '/src/App.vue', 'resources/assets/js/App.vue', false);
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
